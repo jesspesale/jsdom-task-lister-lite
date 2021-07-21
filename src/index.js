c373e2e7d46e7d1c827ab0592c073132828d4332
Jess Pesale <jess_pesale@aol.com>
2021-07-20 22:06:41 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  form = document.getElementById("create-task-form")
+
+  form.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    let input = document.getElementById("new-task-description").value
+    console.log(input)
+
+    let ul = document.getElementById("tasks")
+    let li = document.createElement("li")
+    li.innerHTML = input
+    ul.appendChild(li)
+  }
+  )
 });
